ff811a95107fb4e2b9c456e1a13e1e271b3b15ae
Ines-nanou <ines-habassi@live.fr>
2026-06-23 16:02:32 +0200

    Update generer.js

--- api/generer.js
+++ api/generer.js
@@ -184,8 +184,8 @@ async function pageLettreGarde(pdfDoc, fonts, logoImg, infos, fd) {
   page.drawText('1400 Yverdon-les-Bains', { x: ML, y, font: R, size: 9, color: BLACK }); y -= 12;
   page.drawText('024 426 77 00  ·  info@legato-eg.ch', { x: ML, y, font: R, size: 9, color: BLACK });
 
-  // Adresse destinataire a droite, police 12pt
-  let ey = PH - 215;
+  // Adresse destinataire remontee de ~3.5cm par rapport a avant
+  let ey = PH - 115;
   const nomComplet = `${fd.nomEntreprise} ${fd.formeJuridique}`;
   page.drawText(nomComplet, { x: 340, y: ey, font: B, size: 12, color: BLACK }); ey -= 16;
   if (infos.adresseEntreprise)  { page.drawText(infos.adresseEntreprise,  { x: 340, y: ey, font: R, size: 12, color: BLACK }); ey -= 15; }
